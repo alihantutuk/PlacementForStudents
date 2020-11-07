2a8a5bb82bb5e4979a75235d8f3b1c39a357f68a
ertugrulsmz <ertugrulsmz55@gmail.com>
2020-11-07 10:46:49 +0300

    log-in, sign-up template

---
+++ webapp/db_models.py
@@ -0,0 +1,57 @@
+from webapp import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+#login manager is obj in init.py works as loginmanager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('page_id', db.Integer, db.ForeignKey('page.id'), primary_key=True)
+)
+
+class Page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+        backref=db.backref('pages', lazy=True))
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    addresses = db.relationship('Address', backref='person', lazy=True)
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
+        nullable=False)
+
---
+++ webapp/routes.py
@@ -0,0 +1,89 @@
+from flask import render_template, url_for, flash, redirect, request
+from webapp import app, db, bcrypt
+from webapp.forms import RegistrationForm, LoginForm
+from webapp.db_models import User,Post
+from flask_login import login_user,current_user,logout_user,login_required
+
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/account")
+@login_required #from flask_login package
+def account():
+    return render_template('account.html', title='Account')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect( url_for( 'home' ))
+
+    form = RegistrationForm()
+
+    #came from posted form, if validated
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash( form.password.data ).decode( 'utf-8' )
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password )
+        db.session.add( user )
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect( url_for( 'home' ))
+
+    form = LoginForm()
+
+    #came from posted form, if validated including database.
+    if form.validate_on_submit():
+        user = User.query.filter_by( email=form.email.data ).first()
+        if user and bcrypt.check_password_hash( user.password, form.password.data ):
+            
+            login_user( user, remember=form.remember.data)
+
+            #from where login page is reached
+            next_page = request.args.get( 'next' )
+            if next_page:
+                return redirect(next_page) # url_for or pure string?
+            else:
+                return redirect( url_for( 'home' ) )
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user() #from flask_login package
+    return redirect(url_for('home'))
