4cb8cba1e5874115d135b5aafcc1787b4c41afd4
ertugrulsmz <ertugrulsmz55@gmail.com>
2020-11-25 17:31:12 +0300

    company account page and search bar is added

--- webapp/forms.py
+++ webapp/forms.py
@@ -30,7 +30,7 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
