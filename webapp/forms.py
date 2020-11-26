5d4f471a22eb984e8a4e896911d2bd11d5f3871d
ertugrulsmz <ertugrulsmz55@gmail.com>
2020-11-26 23:17:27 +0300

    company account editing

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from webapp.db_models import User
+from flask_login import current_user
+from webapp import bcrypt
 
 
 
@@ -34,4 +37,42 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class CompanyEditForm(FlaskForm):
+    username = StringField( 'Username',
+                            validators=[DataRequired(), Length( min=2, max=20 )] )
+    email = StringField( 'Email',validators=[DataRequired(), Email()] )
+    password = PasswordField( 'Password', validators=[DataRequired()] )
+    confirm_password = PasswordField( 'Confirm Password',
+                                      validators=[DataRequired(), EqualTo( 'password' )] )
+    website = StringField( 'Website' )
+    linkedin = StringField( 'Linkedin')
+    github = StringField( 'GitHub')
+    name = StringField('Name')
+    address = StringField('Address')
+    image = FileField( 'Image', validators=[FileAllowed( ['jpg', 'png'] )] )
+    description = StringField('description',validators=[DataRequired()])
+    sector = StringField( 'sector', validators=[DataRequired()] )
+    numberofworkers = IntegerField('numberofworkers',validators=[DataRequired()])
+    submit = SubmitField( 'Update' )
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by( username=username.data ).first()
+            if user:
+                raise ValidationError( 'That username is taken. Please choose a different one.' )
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by( email=email.data ).first()
+            if user:
+                raise ValidationError( 'That email is taken. Please choose a different one.' )
+
+    def validate_password(self,password):
+
+        if bcrypt.check_password_hash( current_user.password, password.data ) == False:
+            raise ValidationError( 'That password does not belong to this account' )
+
+
 
